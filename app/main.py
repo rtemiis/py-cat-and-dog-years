d9fe306e42c4475d515a606f08d20bc9974ba692
Artem Kolesnikov <artemiyk2017@gmail.com>
2026-04-23 00:50:53 +0200

    fix: requested changes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
-    if type(cat_age) is not int or type(dog_age) is not int:
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
         raise TypeError("cat_age and dog_age must be integers")
 
     cat_human_age = 0
--- app/test_main.py
+++ app/test_main.py
@@ -7,6 +7,7 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, cat_expected_human_age, dog_expected_human_age",
     [
+        (-1, -1, 0, 0),
         (0, 0, 0, 0),
         (14, 14, 0, 0),
         (15, 15, 1, 1),
@@ -14,6 +15,7 @@ from app.main import get_human_age
         (24, 24, 2, 2),
         (27, 27, 2, 2),
         (28, 28, 3, 2),
+        (29, 29, 3, 3),
         (100, 100, 21, 17)
     ]
 )
@@ -37,8 +39,7 @@ def test_get_human_age(
         None,
         [10, 20],
         {"five": 5},
-        (3, 3),
-        False
+        (3, 3)
     ]
 )
 def test_bad_input(bad_input: Any) -> None:
